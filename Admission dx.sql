537ba62aff9d6c089b8685929e3dbf67a5fd7033
ajwebb3 <128554753+ajwebb3@users.noreply.github.com>
2025-01-20 11:47:29 -0500

    Update Admission dx.sql
    Fixing Snowflake syntax from EDW

--- Admission dx.sql
+++ Admission dx.sql
@@ -2,6 +2,6 @@ select t1.PatientEncounterID,
                             t1.AdmitDiagnosisTXT, 
                             t2.DiagnosisID, 
                             t2.DiagnosisNM, 
-                            t2.CurrentICD10ListTXT from Epic.Encounter.HospitalAdmitDiagnosis_MGH as t1
-                   left join Epic.Reference.ICDDiagnosis as t2 on t1.DiagnosisID = t2.DiagnosisID 
+                            t2.CurrentICD10ListTXT from EDW_SOURCE_ZONE_Epic.Encounter_Clinical.HospitalAdmitDiagnosis as t1
+                   left join EDW_SOURCE_ZONE_Epic.Reference.ICDDiagnosis as t2 on t1.DiagnosisID = t2.DiagnosisID 
                    where PatientEncounterID IN ( )
