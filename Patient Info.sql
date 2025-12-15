a2af4d00733c91930f8a8d30a7bf0c743f4e9612
Andy Webb <128554753+ajwebb3@users.noreply.github.com>
2025-12-15 11:37:52 -0500

    vault backup: 2025-12-15 11:37:52

--- Patient Info.sql
+++ Patient Info.sql
@@ -1,42 +1,24 @@
 /** Updated for Clarity syntax, 12/15/25 **/
 
 SELECT DISTINCT t1.pat_id,
-                t2.pat_enc_csn_id,
-				t1.borth_date,
-				t1.DeathDTS,
-				t2.HospitalAdmitDTS,
-				t2.HospitalDischargeDTS, 
-                DATEDIFF(minute,t2.HospitalAdmitDTS,t2.HospitalDischargeDTS)/1440.0 as HospitalLOS, 
-				DATEDIFF(year,t1.BirthDTS,t2.HospitalAdmitDTS) as AgeNBR, 
-				t2.WeightOunceNBR, 
-				t1.SexCD, 
-				t1.SexDSC,
-				t1.EthnicGroupCD,
-				t1.EthnicGroupDSC,
-				t3.PatientRaceCD,
-				t3.PatientRaceDSC,
-				t3.LineNBR as RaceLineNBR,
-				t1.MaritalStatusCD, 
-				t1.MaritalStatusDSC, 
-				t1.LanguageCD, 
-				t1.LanguageDSC, 
-				t1.InterpreterNeededFLG, 
-				t4.EducationLevelCD, 
-				t4.EducationLevelDSC, 
-				t1.ReligionCD, 
-				t1.ReligionDSC,
-                t1.AddressLine01TXT, 
-				t1.CityNM, 
-				t1.StateDSC, 
-				t1.CountyDSC, 
-				t1.CountryDSC, 
-				t1.ZipCD
+                t3.pat_enc_csn_id,
+				t1.birth_date,
+				t2.death_tm,
+				t3.HOSP_ADMSN_TIME,
+				t3.HOSP_DISCHRG_TIME, 
+				DATEDIFF(year, t1.birth_date, t3.HOSP_ADMSN_TIME) as AgeNBR, 
+				t3.height,
+				t3.weight,
+				t1.sex_c, 
+				t5.name as PATIENT_RACE,
+				t4.LINE as RACE_LINE_NM
 FROM EDW_SOURCE_ZONE_CLARITY.Clinical.Patient t1
-				LEFT JOIN 
-                RIGHT JOIN EDW_Source_Zone_Epic.Encounter_Clinical.PatientEncounter t2 on (t1.PatientID=t2.PatientID)
-                LEFT JOIN EDW_Source_Zone_Epic.Patient_Clinical.Race t3 on (t1.PatientID=t3.PatientID)
-                LEFT JOIN EDW_Source_Zone_Epic.Patient_Clinical.Patient4 t4 on t4.PatientID=t2.PatientID
-WHERE t2.PatientEncounterID in (' ') -- Encounter CSN
+				LEFT JOIN EDW_SOURCE_ZONE_CLARITY.Clinical.Patient_2 t2 on t1.pat_id = t2.pat_id
+                RIGHT JOIN EDW_SOURCE_ZONE_CLARITY.CLINICAL.PAT_ENC t3 on (t1.pat_id = t2.pat_id)
+				LEFT JOIN EDW_SOURCE_ZONE_CLARITY.CLINICAL.PATIENT_RACE t4 on t1.pat_id = t4.pat_id
+				LEFT JOIN EDW_SOURCE_ZONE_CLARITY.COMMON.ZC_PATIENT_RACE t5 on t4.patient_race_c = t5.patient_race_c
+WHERE t3.pat_enc_csn_id in (' ') -- Encounter CSN
+and t4.LINE = 1
 
 /** t2.EncounterTypeCD Dictionary **/
 /**
