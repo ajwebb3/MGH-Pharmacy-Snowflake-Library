97b3483a6fc9fe5d1416a09fd8bc21e5eac536da
Andy Webb <128554753+ajwebb3@users.noreply.github.com>
2025-12-15 13:13:39 -0500

    vault backup: 2025-12-15 13:13:39

---
+++ Problem List.sql
@@ -0,0 +1,10 @@
+select t1.PatientEncounterID, 
+       t1.patientid, 
+	   t1.linenbr,
+	   t1.AdmitDiagnosisTXT, 
+	   t2.DiagnosisID, 
+	   t2.DiagnosisNM, 
+	   t2.CurrentICD10ListTXT 
+from EDW_SOURCE_ZONE_Epic.Encounter_Clinical.HospitalAdmitDiagnosis as t1
+left join EDW_SOURCE_ZONE_Epic.Reference.ICDDiagnosis as t2 on t1.DiagnosisID = t2.DiagnosisID 
+where PatientEncounterID in ()
